b744a606019b7115d9d783812af4efdd631c8ca8
Marek Siekierski <mark.siekierski@gmail.com>
2019-06-22 02:22:45 -0400

    Hello world

--- marekfs/urls.py
+++ marekfs/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, include
 from django.urls import path
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from . import views
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -37,7 +38,6 @@ router.register(r'users', UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', views.index),
     path('admin/', admin.site.urls)
 ]
