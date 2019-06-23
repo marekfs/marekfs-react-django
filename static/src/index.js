1a412a1a4ce7241f033e84549929cd1bc586f9a9
Marek Siekierski <mark.siekierski@gmail.com>
2019-06-23 15:28:22 -0400

    Added styled components

--- static/src/index.js
+++ static/src/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px'
+}
+
+export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`
+};
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+// Use Title and Wrapper like any other React component – except they're styled!
 ReactDOM.render(
-    <div>
-        <h1>This is JSX.</h1>
-    </div>,
-    document.getElementById("root")
+  <Wrapper>
+    <Title>
+      Using styled components.
+    </Title>
+  </Wrapper>,
+  document.getElementById("root")
 );
