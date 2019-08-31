8cb2b7a6e928ab0edc6afd83727f7e45f05e1a22
Marek Siekierski <mark.siekierski@gmail.com>
2019-08-31 01:49:33 -0400

    Removed styled-components package and some other crap

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,45 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import styled from 'styled-components';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const size = {
-  mobileS: '320px',
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
-  desktop: '2560px'
-}
-
-export const device = {
-  mobileS: `(min-width: ${size.mobileS})`,
-  mobileM: `(min-width: ${size.mobileM})`,
-  mobileL: `(min-width: ${size.mobileL})`,
-  tablet: `(min-width: ${size.tablet})`,
-  laptop: `(min-width: ${size.laptop})`,
-  laptopL: `(min-width: ${size.laptopL})`,
-  desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`
-};
-
-// Create a Title component that'll render an <h1> tag with some styles
-const Title = styled.h1`
-  font-size: 3em;
-  text-align: left;
-  color: palevioletred;
-`;
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 2em;
-  background: papayawhip;
-`;
-// Use Title and Wrapper like any other React component – except they're styled!
-ReactDOM.render(
-  <Wrapper>
-    <Title>
-      marekfs
-    </Title>
-  </Wrapper>,
-  document.getElementById("navbar")
-);
+ReactDOM.render(<Title/>, document.getElementById("root"))
